c3daf08c435f2a6299c56492bf9a0cddab429f7b
mrlectus <lilbrown3000@gmail.com>
2024-11-01 12:19:20 +0100

    wrap in suspence

--- app/(splash)/products/page.tsx
+++ app/(splash)/products/page.tsx
@@ -1,10 +1,13 @@
 import { ProductBoard } from "@/components/product-board";
+import { Suspense } from "react";
 
 const ProductPage = () => {
   return (
-    <main className="flex max-h-screen grow flex-col overflow-hidden">
-      <ProductBoard />
-    </main>
+    <Suspense>
+      <main className="flex max-h-screen grow flex-col overflow-hidden">
+        <ProductBoard />
+      </main>
+    </Suspense>
   );
 };
 
